95d39f080a760cd005fff32a057c55fbeebf3da7
Snehameena <sneha.meena93@gmail.com>
2020-11-26 17:16:43 +0800

    Basic Web Crawling

---
+++ Basic Crawling/scrapycrawl/spiders/scarpy_1.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class scrapy_crawl(scrapy.Spider):
+    name = "posts"
+    start_urls = [
+    "https://blog.scrapinghub.com/page/1/"
+    ]
+    
+    def parse(self,response):
+        page = response.url.split('/')[-1]
+        filename = 'posts-%s.html' % page
+        with open(filename,'wb') as f:
+            f.write(response.body)
+    
